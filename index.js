069739c700ead0897cdbdc70c09e1b648afd6f6b
Jazz <112279221+euJazz@users.noreply.github.com>
2023-11-14 23:29:02 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+//Author: https://github.com/euJazz
+
+
+const express = require('express');
+const app = express();
+app.get("/", (request, response) => {
+  const ping = new Date();
+  ping.setHours(ping.getHours() - 3);
+  console.log(`Novo ping em: ${ping.getUTCHours()}:${ping.getUTCMinutes()}:${ping.getUTCSeconds()}`);
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT); 
+
+const Discord = require("discord.js"); 
+const client = new Discord.Client({partials: ["MEMBER", "MESSAGE", "CHANNEL", "USER", "REACTION"]}); 
+const config = require("./config.json"); 
+const db = require("quick.db")
+
+client.login(process.env.TOKEN); 
+
+
+client.on('message', message => {
+     if (message.author.bot) return;
+     if (message.channel.type == 'dm') return;
+     if (!message.content.toLowerCase().startsWith(config.prefix.toLowerCase())) return;
+     if (message.content.startsWith(`<@!${client.user.id}>`) || message.content.startsWith(`<@${client.user.id}>`)) return;
+
+    const args = message.content
+        .trim().slice(config.prefix.length)
+        .split(/ +/g);
+    const command = args.shift().toLowerCase();
+
+    try {
+        const commandFile = require(`./commands/${command}.js`)
+        commandFile.run(client, message, args);
+    } catch (err) {
+    console.error('Erro:' + err);
+  }
+});
+
+
+//Author: https://github.com/euJazz
